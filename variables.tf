7e78320b4ca0385e181a18b41180ea0f303e65e7
Ishita Goel <92685780+Ishuu1124@users.noreply.github.com>
2025-05-02 15:12:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,35 +10,35 @@
 #   - For the same reason as above, do not add default values to the example inputs.
 #
 
-variable "ibmcloud_api_key" {
+variable "apiKey" {
   type        = string
   description = "The IBM Cloud API Key."
   sensitive   = true
 }
 
-variable "region" {
+variable "reg" {
   type        = string
   description = "Region to provision all resources created by this example."
 }
 
-variable "prefix" {
+variable "pfx" {
   type        = string
   description = "A string value to prefix to all resources created by this example."
 }
 
-variable "resource_group" {
+variable "rg" {
   type        = string
   description = "The name of an existing resource group to provision resources in to. If not set a new resource group will be created using the prefix variable."
   default     = null
 }
 
-variable "resource_tags" {
+variable "resTags" {
   type        = list(string)
   description = "List of resource tag to associate with all resource instances created by this example."
   default     = []
 }
 
-variable "access_tags" {
+variable "tags_access" {
   type        = list(string)
   description = "Optional list of access management tags to add to resources that are created."
   default     = []
