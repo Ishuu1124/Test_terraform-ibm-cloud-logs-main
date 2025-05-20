c2d243e100f20704ee25bdd317dc598ae5069927
Ishita Goel <92685780+Ishuu1124@users.noreply.github.com>
2025-05-20 13:30:32 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -30,5 +30,4 @@ output "ingress_private_endpoint" {
 
 output "logs_policies_details" {
   value       = length(var.policies) > 0 ? ibm_logs_policy.logs_policies : null
-  description = "The details of the Cloud logs policies created."
 }
